62a922c728b616cb7b43793e0ca555e5307601dc
samdoiron <sam.doiron96@gmail.com>
2016-02-09 21:06:30 -0400

    Initial commit

--- src/site/core.clj
+++ src/site/core.clj
@@ -12,6 +12,31 @@
    {:url "http://proof.nationalgeographic.com/files/2014/11/PastedGraphic-2.jpg" :color "#132635"}
    {:url "https://drscdn.500px.org/photo/52866292/m=2048_k=1_a=1/2683707825210f5bb6d26e7c32d14738" :color "#687275"}
    {:url "http://proof.nationalgeographic.com/files/2015/04/150501-bestpod-volcANO.jpg" :color "#07213F"}
+   {:url "http://imgur.com/AmWThvw.jpg", :color "gray" }
+   {:url "http://proof.nationalgeographic.com/files/2015/04/150501-bestpod-volcANO.jpg", :color "gray" }
+   {:url "http://proof.nationalgeographic.com/files/2014/11/PastedGraphic-2.jpg", :color "gray" }
+   {:url "http://ppcdn.500px.org/52866292/9e944f286e9ba110135839dca1aff8a18e18ffe8/2048.jpg", :color "gray" }
+   {:url "http://proof.nationalgeographic.com/files/2015/07/NationalGeographic_1203310.jpg", :color "gray" }
+   {:url "https://farm1.staticflickr.com/691/20664938416_4e4b224684_h.jpg", :color "gray" }
+   {:url "http://i.imgur.com/0fR0RmH.jpg", :color "gray" }
+   {:url "http://i.imgur.com/UaQJaKH.jpg", :color "gray" }
+   {:url "http://i.imgur.com/IaEHwyB.jpg", :color "gray" }
+   {:url "http://imgur.com/sATBfbP.jpg", :color "gray" }
+   {:url "http://www.imgur.com/bcvSSgM.jpg", :color "gray" }
+   {:url "http://imgur.com/YY3bLP7.jpg", :color "gray" }
+   {:url "http://imgur.com/P5qucZh.jpg", :color "gray" }
+   {:url "http://i.imgur.com/zynuyMO.jpg", :color "gray" }
+   {:url "http://i.imgur.com/4PC1oo9.jpg", :color "gray" }
+   {:url "http://imgur.com/1SHZTmz.jpg", :color "gray" }
+   {:url "http://i.imgur.com/3f27eza.jpg", :color "gray" }
+   {:url "https://c1.staticflickr.com/1/757/22792566551_ff23233525_o.jpg", :color "gray" }
+   {:url "http://proof.nationalgeographic.com/files/2015/05/5961639_uploadsmember992198yourshot-992198-5961639jpg_ji34qor4k7racdoqsxku5de2whp3eflutfvvbpyjwjhzlmh4iziq_3456x2592.jpg", :color "gray" }
+   {:url "https://c1.staticflickr.com/1/632/21136101110_1dde1c1a7e_o.jpg", :color "gray" }
+   {:url "https://wearestardust.smugmug.com/Astrophotography/i-HMQHmst/0/X3/IMG_2240%20HQ%20FINAL%20Signature-X3.jpg", :color "gray" }
+   {:url "http://i.imgur.com/5on032B.jpg", :color "gray" }
+   {:url "http://farm9.staticflickr.com//8868//18205140545_c48ca49018_h.jpg", :color "gray" }
+   {:url "http://i.imgur.com/yK3VDgP.jpg", :color "gray" }
+   {:url "http://i.imgur.com/nZQuJZM.jpg" :color "gray" }
    ])
 
 (defn string->vec [s] (seq s))
@@ -72,15 +97,7 @@
     [:a.article { "href" "http://www.google.com" "style" (image-style (rand-nth test-images)) }
        [:h1.article-title (:title art)]])
 
-(defn articles [arts]
-  [:div.articles 
-   (navigation [{ :url "#" :title "Projects" }
-                { :url "#" :title "Blog" }
-                { :url "#" :title "Resume" }])
-   (splash)
-   (map article arts)])
-
-(defn splash []
+(def splash
   [:div.splash
    [:div.splash-greeting
      [:img.splash-me { "src" "http://avatars2.githubusercontent.com/u/3770220?v=3&s=460" }]
@@ -98,7 +115,16 @@
     [:span.software-architecture "Software Architecture"]
     [:span.comma ","]
     " and the "
-    [:span.oxford-comma "Oxford Comma"]]])
+    [:span.oxford-comma "Oxford Comma"]
+    "."]])
+
+(defn articles [arts]
+  [:div.articles 
+   (navigation [{ :url "#" :title "Projects" }
+                { :url "#" :title "Blog" }
+                { :url "#" :title "Resume" }])
+   splash
+   (map article arts)])
 
 (defn front-page []
   (basic-page 
